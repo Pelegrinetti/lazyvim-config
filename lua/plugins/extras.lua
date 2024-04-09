bc1d0f130369f587ece5eeb269b35d8ccc803e7d
Pelegrinetti <pelegrinetti.dev@gmail.com>
2024-04-09 00:08:35 -0300

    feat: enable extras settings
    Add json, eslint and prettier extras for formatting and linting

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,4 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
 }
